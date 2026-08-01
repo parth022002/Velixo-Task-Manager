3ad19dca49fb99e42784424e6230df2272935b4a
parth022002 <parthahuja021102@gmail.com>
2026-08-01 21:16:10 +0530

    Support VITE_API_URL and VITE_APP_BASE_URL fallbacks in apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = import.meta.env.VITE_APP_BASE_URL;
+const API_URI = import.meta.env.VITE_APP_BASE_URL || import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
